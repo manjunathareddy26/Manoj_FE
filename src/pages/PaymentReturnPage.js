a59a1aacdac6fb1166f7ceb18b33d7923fdc52fa
manjunathareddy26 <manjunath@ahoosocial.com>
2026-05-05 20:12:34 +0530

    fix: correct import of paymentService for sync endpoint

--- src/pages/PaymentReturnPage.js
+++ src/pages/PaymentReturnPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { CheckCircle, AlertCircle, Loader } from 'lucide-react';
-import * as paymentService from '../services/index';
+import { paymentService } from '../services/index';
 
 const PaymentReturnPage = () => {
   const { appOrderId } = useParams();
